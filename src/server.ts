064343c58b3fa633ac6022b041eae8bfd32e77dc
Alex Careja <Alex.Careja@softwire.com>
2021-06-30 16:29:03 +0300

    cleaned up code, changed lets into consts, added missing semicolon, fixed wrong indentation

--- src/server.ts
+++ src/server.ts
@@ -3,21 +3,21 @@ import express, { Express } from "express";
  
 const expressApp = express();
 const port = 8000;
-const axios = require('axios');
+const axios = require("axios");
 
 function getTest(expressRequest: express.Request, expressResponse: express.Response): void {
     expressResponse.send("Hello wolrd !");
 }
 
 function getRovers(expressRequest: express.Request, expressResponse: express.Response): void {
-    axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=IqA8fYkIhjXf62lkAuWM3WqexG4waX0mbovOUeII')
+    axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=IqA8fYkIhjXf62lkAuWM3WqexG4waX0mbovOUeII")
         .then(function (axiosResponse: AxiosResponse) {
-        let roverNamesList: string[] = [];
+            const roverNamesList: string[] = [];
             for (const rover of axiosResponse["data"]["rovers"]) {
                 roverNamesList.push(rover["name"]);
             }
-        let roverNamesString: string = roverNamesList.join(", ")
-        expressResponse.send(roverNamesString);
+            const roverNamesString = roverNamesList.join(", ");
+            expressResponse.send(roverNamesString);
         })
         .catch(function (error: string) {
             console.log(error);
@@ -29,8 +29,8 @@ function getRovers(expressRequest: express.Request, expressResponse: express.Res
 function initRouter(): express.Router {
     const router = express.Router();
 
-    router.get('/test', getTest);
-    router.get('/rovers', getRovers);
+    router.get("/test", getTest);
+    router.get("/rovers", getRovers);
 
     return router;
 }
@@ -41,7 +41,7 @@ expressApp.use(express.json());
 
 const router = initRouter();
 
-expressApp.use('/', router);
+expressApp.use("/", router);
  
 expressApp.listen(port, () => {
   console.log(`Test backend is running on port ${port}`);
