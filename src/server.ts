94ac3a57a63040c3661d45e6b88a45dc265c7f47
Alex Careja <Alex.Careja@softwire.com>
2021-06-30 14:22:31 +0300

    Added a new route '/rovers' where it prints out the names of the Mars rovers; the names are collected using a get request to the NASA API

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,33 @@
+import { AxiosResponse } from "axios";
 import express, { Express } from "express";
  
-const app = express();
+
+const axios = require('axios');
+const expressApp = express();
 const port = 8000;
+
  
-app.use(express.json());
+expressApp.use(express.json());
 const router = express.Router();
-router.get('/test', (req, res) => res.send('Hello world !'));
-app.use('/', router);
+router.get('/test', (expressRequest: express.Request, expressResponse: express.Response) => {
+    axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=IqA8fYkIhjXf62lkAuWM3WqexG4waX0mbovOUeII')
+        .then(function (axiosResponse: AxiosResponse) {
+        let roverNamesList: string[] = [];
+            for (const rover of axiosResponse["data"]["rovers"]) {
+                roverNamesList.push(rover["name"]);
+            }
+        let roverNamesString: string = roverNamesList.join(", ")
+        expressResponse.send(roverNamesString);
+        })
+        .catch(function (error: string) {
+            console.log(error);
+        })
+        .then(function () {
+        });
+        
+});
+expressApp.use('/', router);
  
-app.listen(port, () => {
+expressApp.listen(port, () => {
   console.log(`Test backend is running on port ${port}`);
 });
